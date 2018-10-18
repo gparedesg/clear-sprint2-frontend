da956d746f23d9a5443d08067a62cbc8f8d40fc8
Gabriel Paredes <gabriel@axented.com>
2018-10-18 17:42:11 -0400

    Add Material Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule } from '@angular/material';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+  	BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  exports: [
+  	RouterModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule
+  ]
 })
 export class AppRoutingModule { }
